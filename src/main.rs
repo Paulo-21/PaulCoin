a1020bbb22fb74500d6d1138bc0fb91b404b945c
paul.cibier <paul.cibier@gmail.com>
2021-11-28 00:38:41 +0100

    first node code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,51 @@
+use tokio::net::{TcpListener, TcpStream};
+use std::io;
+use std::fs::File;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::collections::HashMap;
 
-
-fn handle_client(stream: TcpStream) {
-    // ...
-}
-
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:34254")?;
-
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
+mod server;
+async fn start_client(mut ip : String) {
+    ip.push_str(":80");
+    let mut client = TcpStream::connect(&ip[..]).await;
+    match client {
+        Ok(c) => {
+            println!("Connected : {}", ip);
+            loop {
+                c.writable().await;
+                match c.try_write(b"hello world") {
+                    Ok(n) => {
+                        break;
+                    }
+                    Err(_e) => {
+                        continue;
+                    }
+                }
+            }
+        }
+        Err(err) => { println!("{}", err); }
     }
     
+}
+#[tokio::main]
+async fn main() {
+    let all_tcp: HashMap<&str, TcpStream> = HashMap::new();
+    
+    let mut file = File::open("votant.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+    println!("{}", contents);
+    let clients = contents.split(' ').collect::<Vec<_>>();
+    
+    let mut client_spanw = Vec::new();
 
-    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
-
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
-    Ok(())
+    for ip in clients {
+        let a = String::from(ip).clone();
+        client_spanw.push(tokio::spawn( start_client(a) ));
+    }
+    let server = tokio::spawn(server::start_server());
+    server.await;
+    for x in client_spanw {
+        x.await;
+    }
 }
---
+++ src/server.rs
@@ -0,0 +1,39 @@
+use tokio::net::{TcpListener, TcpStream};
+
+pub async fn process(stream: TcpStream) {
+
+    let mut msg = vec![0;1024];
+    loop {
+        stream.readable().await;
+
+        match stream.try_read(&mut msg) {
+            Ok(n) => {
+                msg.truncate(n);
+                break;
+            }
+            Err(e) => {
+                continue;
+            }
+        }
+    }
+    println!("Receive = {:?}", msg);
+}
+
+pub async fn start_server() {
+    let listener = TcpListener::bind("0.0.0.0:80").await;
+    match listener {
+        Ok(listen) => {
+            println!("SERVER START SUCCESSFUL");
+            loop {
+                let (socket, _) = listen.accept().await.unwrap();
+                tokio::spawn(async {
+                    process(socket).await;
+                });
+            }
+        }
+        Err (err) => {
+            println!("{}", err);
+        }
+    }
+    
+}
