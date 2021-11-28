f83c52353b6bf134b880649f0189a9bf2dc61871
Paulo <paul.cibier@gmail.com>
2021-11-28 20:41:51 +0100

    resolve some warnings and clean code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use tokio::net::{TcpListener, TcpStream};
-use std::io;
+use tokio::net::{TcpStream};
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
@@ -14,12 +13,11 @@ async fn manager ( mut all_tcp : HashMap<SocketAddr, TcpStream> , mut rx: Receiv
             loop {
                 stream.writable().await;
                 match stream.try_write(b"hello world") {
-                    Ok(n) => { break; }
+                    Ok(_n) => { break; }
                     Err(_e) => { continue; }
                 }
             }
             all_tcp.insert(stream.peer_addr().unwrap(), stream);
-            println!("Hello");
         }
         println!("Start show all tcp");
         for (x, stream) in all_tcp {
@@ -29,7 +27,7 @@ async fn manager ( mut all_tcp : HashMap<SocketAddr, TcpStream> , mut rx: Receiv
 }
 async fn start_client(mut ip : String, tx  : Sender<TcpStream>) {
     ip.push_str(":80");
-    let mut stream = TcpStream::connect(&ip[..]).await;
+    let stream = TcpStream::connect(&ip[..]).await;
     match stream {
         Ok(c) => {
             println!("Connected : {}", ip);
@@ -44,29 +42,28 @@ async fn start_client(mut ip : String, tx  : Sender<TcpStream>) {
         }
         Err(err) => { println!("{}", err); }
     }
-    drop(tx);
     println!("client destroyed");
 }
 #[tokio::main]
 async fn main() {
-    let mut all_tcp: HashMap<SocketAddr, TcpStream> = HashMap::new();
-    let (tx , mut rx) : (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel(32);
+    let all_tcp: HashMap<SocketAddr, TcpStream> = HashMap::new();
+    let (tx , rx) : (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel(32);
     let mut file = File::open("votant.txt").unwrap();
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    file.read_to_string(&mut contents).unwrap();
     println!("{}", contents);
     let clients = contents.split(' ').collect::<Vec<_>>();
     
     let mut client_spanw = Vec::new();
-    let _manager = tokio::spawn(manager(all_tcp, rx));
+    let _manager = tokio::spawn(manager(all_tcp, rx) );
     
     for ip in clients {
         let tx2 = tx.clone();
         let a = String::from(ip).clone();
-        client_spanw.push(tokio::spawn( start_client(a, tx2) ));
+        client_spanw.push( tokio::spawn(start_client(a, tx2)));
     }
-    let server = tokio::spawn(server::start_server());
-    server.await;
+    let server = tokio::spawn(server::start_server() );
+    server.await.unwrap();
     for x in client_spanw {
         x.await;
     }
