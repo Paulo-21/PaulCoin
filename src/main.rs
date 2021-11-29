c7b574cd25f99679dd07bf7d1f7b9114e5e0f544
paul.cibier <paul.cibier@gmail.com>
2021-11-29 22:33:37 +0100

    Some code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio::net::{TcpStream};
+use tokio::net::{TcpListener, TcpStream, tcp::OwnedReadHalf, tcp::OwnedWriteHalf};
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
@@ -15,45 +15,15 @@ enum Commande {
     }
 }
 mod server;
-async fn receive_loop(tx : mpsc::Sender<Commande>) {
-    loop {
 
-    }
+async fn stream_reader(stream : OwnedReadHalf) {
+
 }
-async fn stream_main(stream : TcpStream, mut rx_mpsc_receive_loop : mpsc::Receiver<Commande>, rx_watch_manager : watch::Receiver<Commande>, tx_mpsc_manager: watch::Receiver<Commande>) {
-    while let Some(cmd) = rx.recv().await {
-        use Commande::*;
-        match cmd {
-            Send { value } => {
-                loop {
-                    stream.writable().await;
-                    match stream.try_write(value.as_bytes()) {
-                        Ok(_n) => { break; }
-                        Err(_e) => { continue; }
-                    }
-                }
-            }
-            Receive { value } => {
-                let mut msg = vec![0;1024];
-                loop {
-                    stream.readable().await;
-                    match stream.try_read(&mut msg) {
-                        Ok(n) => {
-                            msg.truncate(n);
-                            tokio::spawn(async move {
-                                process_receive(msg).await;
-                            });
-                            break;
-                        }
-                        Err(_e) => {
-                            continue;
-                        }
-                    }
-                }
-            }
-        }
-    }
+async fn stream_writer(stream : OwnedWriteHalf) {
+
 }
+
+
 async fn process_receive(msg : Vec<u8>) {
     let c: &[u8] = &msg;
     println!("PROCESS : {}", String::from_utf8_lossy(c));
@@ -69,15 +39,67 @@ async fn manager ( mut rx_server: mpsc::Receiver<Commande> , mut tx_streams : wa
         }*/
     }
 }
-async fn process_stream (stream : TcpStream, tx_mpsc_manager: mpsc::Sender<Commande>, rx_watch_manager: watch::Receiver<Commande>) {
-    let (tx, rx_mpsc_receive_loop) = mpsc::channel(20); //Receiver loop and stream_main
+async fn process_new_connection (mut stream : TcpStream, tx_mpsc_manager: mpsc::Sender<Commande>) {
+    let (tx, rx_mpsc_receive_loop) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(20);
+    let (reader, writer) = stream.into_split();
     tokio::spawn(async move {
-        stream_main(stream, rx_mpsc_receive_loop, tx_mpsc_manager, rx_watch_manager).await;
+        stream_writer(writer).await;
     });
     tokio::spawn( async move {
-        receive_loop(tx).await;
+        stream_reader(reader).await;
     });
+    tx_mpsc_manager.send(Commande::Send{value: String::from("ADD")});
 }
+
+
+
+async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>) {
+    let listener = TcpListener::bind("0.0.0.0:80").await;
+    match listener {
+        Ok(listen) => {
+            println!("SERVER START SUCCESSFUL");
+            loop {
+                let (socket, _) = listen.accept().await.unwrap();
+                let tx_mpsc_manager_clone =  tx_mpsc_manager.clone();
+                tokio::spawn(async {
+                    process_new_connection(socket, tx_mpsc_manager_clone).await;
+                });
+            }
+        }
+        Err (err) => {
+            println!("{}", err);
+        }
+    }
+    
+}
+#[tokio::main]
+async fn main() {
+    
+    let (tx_watch , rx_watch) : (watch::Sender<Commande>, watch::Receiver<Commande>) = watch::channel(Commande::Send{value:String::from("Bonjour")});
+    let (tx_mpsc_manager , rx_mpsc_manager) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(32);
+    let mut file = File::open("votant.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}", contents);
+    let clients = contents.split(' ').collect::<Vec<_>>();
+    
+    let _manager = tokio::spawn(manager(rx_mpsc_manager, tx_watch) );
+    //let mut client_spanw = Vec::new();
+    
+    /*for ip in clients {
+        let tx2 = tx.clone();
+        let a = String::from(ip).clone();
+        client_spanw.push( tokio::spawn(start_client(a, tx2)));
+    }*/
+    let server = tokio::spawn(start_server(tx_mpsc_manager) );
+    server.await.unwrap();
+    /*for x in client_spanw {
+        x.await;
+    }*/
+    _manager.await;
+    
+}
+
 async fn start_client(mut ip : String, tx  : mpsc::Sender<TcpStream>) {
     ip.push_str(":80");
     let stream = TcpStream::connect(&ip[..]).await;
@@ -97,30 +119,22 @@ async fn start_client(mut ip : String, tx  : mpsc::Sender<TcpStream>) {
     }
     println!("client destroyed");
 }
-#[tokio::main]
-async fn main() {
-    
-    let (tx_watch , rx_watch) : (watch::Sender<Commande>, watch::Receiver<Commande>) = watch::channel(Commande::Send{value:String::from("Bonjour")});
-    let (tx_mpsc , rx_mpsc) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(32);
-    let mut file = File::open("votant.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("{}", contents);
-    let clients = contents.split(' ').collect::<Vec<_>>();
-    
-    //let mut client_spanw = Vec::new();
-    let _manager = tokio::spawn(manager(rx_mpsc, tx_watch) );
-    
-    /*for ip in clients {
-        let tx2 = tx.clone();
-        let a = String::from(ip).clone();
-        client_spanw.push( tokio::spawn(start_client(a, tx2)));
-    }*/
-    let server = tokio::spawn(server::start_server() );
-    server.await.unwrap();
-    /*for x in client_spanw {
-        x.await;
-    }*/
-    _manager.await;
-    
+
+pub async fn process(stream: TcpStream) {
+
+    let mut msg = vec![0;1024];
+    loop {
+        stream.readable().await;
+
+        match stream.try_read(&mut msg) {
+            Ok(n) => {
+                msg.truncate(n);
+                break;
+            }
+            Err(e) => {
+                continue;
+            }
+        }
+    }
+    println!("Receive = {:?}", msg);
 }
