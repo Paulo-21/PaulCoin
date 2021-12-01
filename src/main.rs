de01799ef9b9cea59a9929e7d4c2ae5c273ca973
Paulo <paul.cibier@gmail.com>
2021-12-01 12:12:20 +0100

    Refactor stream_writer and manager

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-use tokio::net::{TcpListener, TcpStream, tcp::OwnedReadHalf, tcp::OwnedWriteHalf};
+use tokio::net::{ TcpListener, TcpStream, tcp::OwnedReadHalf, tcp::OwnedWriteHalf };
 use std::fs::File;
 use std::io::prelude::*;
-use tokio::sync::{mpsc, watch };
+use tokio::sync::{ mpsc };
+use std::io;
 //use std::net::SocketAddr;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum Commande {
     Send {
         value:String
     },
-    Receive {
-        value:String
+    AddStream {
+        writer_stream : mpsc::Sender<Commande>
     }
+    
 }
 mod server;
-
+async fn stream_writer(stream : OwnedWriteHalf, mut rx : mpsc::Receiver<Commande>) {
+    while let Some(cmd) = rx.recv().await {
+        use Commande::*;
+        match cmd {
+            Send { value } => {
+                loop {
+                    stream.writable().await;
+                    match stream.try_write(value.as_bytes()) {
+                        Ok(n) => {
+                            break;
+                        }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            continue;
+                        }
+                        Err(e) => {
+                            break;
+                        }
+                    }
+                }
+            },
+            _ => {
+                continue;
+            }
+        }
+    }
+}
 async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Commande>) {
     loop {
         let mut msg = vec![0;1024];
@@ -23,12 +50,9 @@ async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Com
         match stream.try_read(&mut msg) {
             Ok(0) => { 
                 println!("connection close");
-                break; 
+                break;
             }
             Ok(n) => {
-                /*if stream.local_addr().is_ok() {
-                    continue;
-                }*/
                 msg.truncate(n);
                 let tx_mpsc_manager_clone = tx_mpsc_manager.clone();
                 tokio::spawn(async move { 
@@ -42,43 +66,25 @@ async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Com
         }
     }
 }
-async fn stream_writer(stream : OwnedWriteHalf, mut rx_watch_manager: watch::Receiver<Commande>) {
-    while rx_watch_manager.changed().await.is_ok() {
-            use Commande::*;
-            //Je commence à bien maitriser
-            let c = { (*rx_watch_manager.borrow()).clone() };
-            match c {
-                Send {value} => {
-                    loop {
-                        stream.writable().await;
-                        //Optimisation à faire
-                        //Value.as_bytes depuis le manager ou en tant que valeur de Command
-                        match stream.try_write(value.as_bytes()) {
-                            Ok(n) => { break; }
-                            Err(_e) => { continue; }
-                        }
-                    }
-                },
-                _ => { continue; }
-            }
-             
-        }
-}
 
 
 async fn process_receive(msg : Vec<u8>, tx_mpsc_manager : mpsc::Sender<Commande>) {
     let c: &[u8] = &msg;
     let str = String::from_utf8_lossy(c);
     tx_mpsc_manager.send(Commande::Send{value : String::from(str)}).await;
-    
 }
-async fn manager ( mut rx_server: mpsc::Receiver<Commande> , mut tx_watch : watch::Sender<Commande>) {
+async fn manager ( mut rx_server: mpsc::Receiver<Commande>) {
+    let mut writers: Vec<mpsc::Sender<Commande>> = Vec::new();
     while let Some(cmd) = rx_server.recv().await {
         use Commande::*;
         match cmd {
             Send { value } => {
-                println!("Manager Receive : {}", value);
-                tx_watch.send(Send { value : value});
+                for tx in &writers {
+                    tx.send(Commande::Send{value : value.clone()}).await;
+                }
+            },
+            AddStream { writer_stream } => {
+                writers.push(writer_stream);
             }
             _ => {
                 continue;
@@ -87,19 +93,21 @@ async fn manager ( mut rx_server: mpsc::Receiver<Commande> , mut tx_watch : watc
     }
 }
 
-async fn process_new_connection (stream : TcpStream, tx_mpsc_manager: mpsc::Sender<Commande>, rx_watch_manager: watch::Receiver<Commande>) {
+async fn process_new_connection (stream : TcpStream, tx_mpsc_manager: mpsc::Sender<Commande>) {
     let (reader, writer) = stream.into_split();
     let tx_mpsc_manager_clone = tx_mpsc_manager.clone();
-    tokio::spawn(async move {
-        stream_writer(writer, rx_watch_manager).await;
+    let (tx, rx) = mpsc::channel(32);
+    tokio::spawn( async move {
+        stream_writer(writer, rx).await;
     });
     tokio::spawn( async move {
         stream_reader(reader, tx_mpsc_manager_clone).await;
     });
-    tx_mpsc_manager.send(Commande::Send{value: String::from("ADD")});
+
+    tx_mpsc_manager.send(Commande::AddStream{writer_stream : tx}).await;
 }
 
-async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>, rx_watch_manager: watch::Receiver<Commande>) {
+async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>) {
     let listener = TcpListener::bind("0.0.0.0:80").await;
     match listener {
         Ok(listen) => {
@@ -107,10 +115,9 @@ async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>, rx_watch_manager:
             loop {
                 let (socket, addr) = listen.accept().await.unwrap();
                 let tx_mpsc_manager_clone =  tx_mpsc_manager.clone();
-                let rx_watch_manager_clone = rx_watch_manager.clone();
                 println!("Receive connection from : {}", addr);
                 tokio::spawn(async {
-                    process_new_connection(socket, tx_mpsc_manager_clone, rx_watch_manager_clone).await;
+                    process_new_connection(socket, tx_mpsc_manager_clone).await;
                 });
             }
         }
@@ -121,7 +128,6 @@ async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>, rx_watch_manager:
 }
 #[tokio::main]
 async fn main() {
-    let (tx_watch ,  rx_watch) : (watch::Sender<Commande>, watch::Receiver<Commande>) = watch::channel(Commande::Send{value:String::from("Bonjour")});
     let (tx_mpsc_manager ,  rx_mpsc_manager) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(32);
     let mut file = File::open("votant.txt").unwrap();
     let mut contents = String::new();
@@ -130,31 +136,32 @@ async fn main() {
     let clients = contents.split(' ').collect::<Vec<_>>();
     
     let _manager = tokio::spawn(async move{
-        manager(rx_mpsc_manager, tx_watch).await;
+        manager(rx_mpsc_manager).await;
     });
     
     for ip in clients {
         let tx_mpsc_manager_clone = tx_mpsc_manager.clone();
-        let rx_watch_clone = rx_watch.clone();
         let a = String::from(ip).clone();
         tokio::spawn(async move {
-            start_client(a, tx_mpsc_manager_clone, rx_watch_clone).await;
+            start_client(a, tx_mpsc_manager_clone).await;
         });
     }
-    let server = tokio::spawn(start_server(tx_mpsc_manager, rx_watch) );
+    let server = tokio::spawn(start_server(tx_mpsc_manager) );
     server.await.unwrap();
     _manager.await;
 }
 
-async fn start_client(mut ip : String, tx_mpsc_manager : mpsc::Sender<Commande>, rx_watch_manager: watch::Receiver<Commande>) {
+async fn start_client(mut ip : String, tx_mpsc_manager : mpsc::Sender<Commande>) {
     ip.push_str(":80");
     match TcpStream::connect(&ip[..]).await {
         Ok(stream) => {
+            if stream.local_addr().unwrap() == stream.peer_addr().unwrap() {
+                return;
+            }
             println!("Connected : {}", ip);
             let tx_mpsc_manager_clone =  tx_mpsc_manager.clone();
-            let rx_watch_manager_clone = rx_watch_manager.clone();
             tokio::spawn(async {
-                process_new_connection(stream, tx_mpsc_manager_clone, rx_watch_manager_clone).await;
+                process_new_connection(stream, tx_mpsc_manager_clone).await;
             });
         }
         Err(err) => { println!("{}", err); }
