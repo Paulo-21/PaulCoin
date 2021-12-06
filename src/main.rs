71dc4a86c5664879d8750b43eccb8de964fe7dc7
Paulo <paul.cibier@gmail.com>
2021-12-06 01:12:38 +0100

    Continue integration of framing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use tokio::net::{ TcpListener, TcpStream, tcp::OwnedReadHalf, tcp::OwnedWriteHalf };
+//use tokio::io::{self, AsyncWriteExt};
 use std::fs::File;
 use std::io::prelude::*;
 use tokio::sync::{ mpsc };
 use std::io;
+use bytes::{BytesMut, BufMut, Buf};
+use std::env;
 //use std::net::SocketAddr;
 
 #[derive(Debug)]
@@ -15,21 +18,96 @@ enum Commande {
     }
     
 }
+enum Frame {
+    Message(String),
+}
+fn get_u8(buffer : &mut dyn Buf) -> Option<u8>{
+    if buffer.has_remaining() {
+        let ret = buffer.chunk()[0];
+        buffer.advance(1);
+        Some(ret)
+    }
+    else {
+        None
+    }
+    
+}
+impl Frame {
+    fn check(buffer : &mut dyn Buf) -> Result<(), &str>{
+        
+        match get_u8(buffer) {
+            Some(n) => {
+                println!("get_u8 {}", n);
+                Ok(())
+            }
+            None  => {
+                Err("nothing")
+            }
+        }
+    }
+    fn parse(buffer : &mut dyn Buf) -> () {
+
+
+    }
+}/*
+fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
+    if src.remaining() < n {
+        return Err(Error::Incomplete);
+    }
+
+    src.advance(n);
+    Ok(())
+}*/
+fn parse_frame(buffer : &mut dyn Buf) -> Option<Frame>{
+    match Frame::check(buffer) {
+        Ok(_) => {
+            Frame::parse(buffer);
+            //let line = get_line(src).to_vec();
+            //let string = String::from_utf8(line);
+            Some(Frame::Message(String::from("fe")))
+        }
+        Err(e) => {
+            None
+        }
+    }
+
+}
+async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Commande>) {
+    let mut buf = BytesMut::with_capacity(1024);
+    
+    loop {
+        if let frame = parse_frame(&mut buf) {
+            //return Ok(Some(frame));
+        }
+        
+        match stream.try_read_buf(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                println!("read {} bytes", n);
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return ;
+            }
+        }
+    }
+}
 async fn stream_writer(stream : OwnedWriteHalf, mut rx : mpsc::Receiver<Commande>) {
     while let Some(cmd) = rx.recv().await {
         use Commande::*;
         match cmd {
             Send { value } => {
                 loop {
+                    let mut data = vec![b'S'];
+                    let vec = value.as_bytes();
+                    data.extend(vec);
                     stream.writable().await;
-                    match stream.try_write(value.as_bytes()) {
-                        Ok(n) => {
-                            //println!("Writer {}", n);
-                            break;
-                        }
-                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                            continue;
-                        }
+
+                    match stream.try_write(&data) {
+                        Ok(n) => { break; }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => { continue; }
                         Err(e) => {
                             println!("{}", e);
                             return;
@@ -39,36 +117,6 @@ async fn stream_writer(stream : OwnedWriteHalf, mut rx : mpsc::Receiver<Commande
             },
             _ => {
                 continue;
-            }
-        }
-    }
-}
-async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Commande>) {
-    loop {
-        let mut msg = vec![0;1024];
-        stream.readable().await;
-
-        match stream.try_read(&mut msg) {
-            Ok(0) => {
-                println!("connection close from {}", stream.peer_addr().unwrap());
-                break;
-            },
-            Ok(n) => {
-                msg.truncate(n);
-                //println!("read {} bytes", n);
-                let tx_mpsc_manager_clone = tx_mpsc_manager.clone();
-                tokio::spawn(async move { 
-                    process_receive(msg, tx_mpsc_manager_clone).await; 
-                });
-            },
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                //println!("BLOCKED READER");
-                continue;
-            }
-            Err(e) => {
-                //println!("{}", e);
-                break;
-                //return;
             }
         }
     }
@@ -136,6 +184,8 @@ async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>) {
 }
 #[tokio::main]
 async fn main() {
+    let arg = env::args().next();
+        
     let (tx_mpsc_manager ,  rx_mpsc_manager) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(32);
     let mut file = File::open("votant.txt").unwrap();
     let mut contents = String::new();
@@ -150,22 +200,38 @@ async fn main() {
     for ip in clients {
         let tx_mpsc_manager_clone = tx_mpsc_manager.clone();
         let a = String::from(ip).clone();
+        let b =  arg.clone();
         tokio::spawn(async move {
-            start_client(a, tx_mpsc_manager_clone).await;
+            start_client(a, tx_mpsc_manager_clone, b).await;
         });
     }
-    let server = tokio::spawn(start_server(tx_mpsc_manager) );
+    let server = tokio::spawn( start_server(tx_mpsc_manager) );
     server.await.unwrap();
     _manager.await.unwrap();
 }
 
-async fn start_client(mut ip : String, tx_mpsc_manager : mpsc::Sender<Commande>) {
+async fn start_client(mut ip : String, tx_mpsc_manager : mpsc::Sender<Commande>, message : Option<String>) {
     ip.push_str(":80");
     match TcpStream::connect(&ip[..]).await {
         Ok(stream) => {
+            
             println!("{}, {}", stream.local_addr().unwrap().ip() , stream.peer_addr().unwrap().ip() );
             if stream.local_addr().unwrap().ip() == stream.peer_addr().unwrap().ip() {
                 return;
+            }
+            if message.is_some() {
+                let a = message.unwrap();
+                loop {
+                    stream.writable().await;
+                    match stream.try_write(a.as_bytes()) {
+                        Ok(n) => { break; }
+                        Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => { continue; }
+                        Err(e) => {
+                            println!("Error : {}", e);
+                            return;
+                        }
+                    }
+                }
             }
             println!("Connected : {}", ip);
             let tx_mpsc_manager_clone =  tx_mpsc_manager.clone();
