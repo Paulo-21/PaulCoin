6b93c49a34ee4ea9d9170c929430ea008572509c
paul.cibier <paul.cibier@gmail.com>
2021-12-07 12:38:04 +0100

    success

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,86 @@ enum Commande {
 enum Frame {
     Message(String),
 }
-fn get_u8(buffer : &mut dyn Buf) -> Option<u8>{
-    if buffer.has_remaining() {
-        let ret = buffer.chunk()[0];
-        buffer.advance(1);
-        Some(ret)
+fn get_u8(buffer : &mut dyn Buf) -> Option<u8> {
+    if !buffer.has_remaining() {
+        return None;
     }
-    else {
-        None
-    }
+    let ret = buffer.chunk()[0];
+    Some(ret)
     
 }
+fn get_str (buffer : &mut dyn Buf) -> Option<String> {
+    let mut i = 0;
+    let mut flag = false;
+    let mut a = String::new(); 
+    for x in buffer.chunk() {
+        a.push(*x as char);
+        if *x == 3 {
+            flag = true;
+            break;
+        }
+        i+=1;
+    }
+    
+    if flag {
+        a.remove(0);
+        a.pop();
+        buffer.advance(i+1);
+        return Some(a);
+    }
+    None
+}
 impl Frame {
-    fn check(buffer : &mut dyn Buf) -> Result<(), &str>{
+    fn check(buffer : &mut dyn Buf) -> Result<String, &str>{
         
         match get_u8(buffer) {
+            Some(1) => { //begin
+                println!("Begin");
+                match get_str(buffer) {
+                    Some(str) => {
+                        println!("get_str : {}", str);
+                        Ok(str)
+                    },
+                    None => {
+                        Err("nothing")
+                    }
+                }
+            }
+            Some(3) => { //End
+                println!("End");
+                Err("oups")
+            },
             Some(n) => {
                 println!("get_u8 {}", n);
-                Ok(())
+                Err("dinguerie")
             }
             None  => {
                 Err("nothing")
             }
         }
     }
-    fn parse(buffer : &mut dyn Buf) -> () {
+    
+}
+/*
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    
+    let start = src.position() as usize;
+    // Scan to the second to last byte
+    let end = src.get_ref().len() - 1;
 
+    for i in start..end {
+        if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
+            // We found a line, update the position to be *after* the \n
+            src.set_position((i + 2) as u64);
 
+            // Return the line
+            return Ok(&src.get_ref()[start..i]);
+        }
     }
-}/*
+
+    Err(Error::Incomplete)
+}*/
+/*
 fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
     if src.remaining() < n {
         return Err(Error::Incomplete);
@@ -60,11 +111,8 @@ fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
 }*/
 fn parse_frame(buffer : &mut dyn Buf) -> Option<Frame>{
     match Frame::check(buffer) {
-        Ok(_) => {
-            Frame::parse(buffer);
-            //let line = get_line(src).to_vec();
-            //let string = String::from_utf8(line);
-            Some(Frame::Message(String::from("fe")))
+        Ok(frame) => {
+            Some(Frame::Message(frame))
         }
         Err(e) => {
             None
@@ -76,10 +124,20 @@ async fn stream_reader(stream : OwnedReadHalf, tx_mpsc_manager: mpsc::Sender<Com
     let mut buf = BytesMut::with_capacity(1024);
     
     loop {
-        if let frame = parse_frame(&mut buf) {
-            //return Ok(Some(frame));
+        match parse_frame(&mut buf) {
+            Some(Frame::Message(message)) => {
+                let cloned = tx_mpsc_manager.clone();
+                tokio::spawn(async move {
+                    process_receive(message.as_bytes(), cloned).await;
+                });
+            }
+            None => {
+
+            }
+            
         }
-        
+        println!("Buffer : {:?} ", buf);
+        stream.readable().await;
         match stream.try_read_buf(&mut buf) {
             Ok(0) => break,
             Ok(n) => {
@@ -100,9 +158,10 @@ async fn stream_writer(stream : OwnedWriteHalf, mut rx : mpsc::Receiver<Commande
         match cmd {
             Send { value } => {
                 loop {
-                    let mut data = vec![b'S'];
+                    let mut data = vec![1];
                     let vec = value.as_bytes();
                     data.extend(vec);
+                    data.extend([3]);
                     stream.writable().await;
 
                     match stream.try_write(&data) {
@@ -123,9 +182,9 @@ async fn stream_writer(stream : OwnedWriteHalf, mut rx : mpsc::Receiver<Commande
 }
 
 
-async fn process_receive(msg : Vec<u8>, tx_mpsc_manager : mpsc::Sender<Commande>) {
-    let c: &[u8] = &msg;
-    let str = String::from_utf8_lossy(c);
+async fn process_receive(msg : &[u8], tx_mpsc_manager : mpsc::Sender<Commande>) {
+    
+    let str = String::from_utf8_lossy(msg);
     tx_mpsc_manager.send(Commande::Send{value : String::from(str)}).await;
 }
 async fn manager ( mut rx_server: mpsc::Receiver<Commande>) {
@@ -184,8 +243,8 @@ async fn start_server(tx_mpsc_manager: mpsc::Sender<Commande>) {
 }
 #[tokio::main]
 async fn main() {
-    let arg = env::args().next();
-        
+    let arg = env::args().last();
+    
     let (tx_mpsc_manager ,  rx_mpsc_manager) : (mpsc::Sender<Commande>, mpsc::Receiver<Commande>) = mpsc::channel(32);
     let mut file = File::open("votant.txt").unwrap();
     let mut contents = String::new();
@@ -215,15 +274,20 @@ async fn start_client(mut ip : String, tx_mpsc_manager : mpsc::Sender<Commande>,
     match TcpStream::connect(&ip[..]).await {
         Ok(stream) => {
             
-            println!("{}, {}", stream.local_addr().unwrap().ip() , stream.peer_addr().unwrap().ip() );
+            /*println!("{}, {}", stream.local_addr().unwrap().ip() , stream.peer_addr().unwrap().ip() );
             if stream.local_addr().unwrap().ip() == stream.peer_addr().unwrap().ip() {
                 return;
-            }
+            }*/
             if message.is_some() {
                 let a = message.unwrap();
+                let mut data = vec![1];
+                let vec = a.as_bytes();
+                data.extend(vec);
+                data.extend([3]);
+                println!("client : {}", a);
                 loop {
                     stream.writable().await;
-                    match stream.try_write(a.as_bytes()) {
+                    match stream.try_write(&data) {
                         Ok(n) => { break; }
                         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => { continue; }
                         Err(e) => {
