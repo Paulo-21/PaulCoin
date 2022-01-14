3bd4148967dcef26c08bda332e41926ca0344d64
paul.cibier <paul.cibier@gmail.com>
2022-01-14 14:54:40 +0100

    continue

--- src/mongo.rs
+++ src/mongo.rs
@@ -2,33 +2,31 @@ use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::{doc, Document};
 
 pub async fn init() {
-    //console_subscriber::init();
     // Parse a connection string into an options struct.
     let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
 
     // Manually set an option.
-    client_options.app_name = Some("My App".to_string());
+    client_options.app_name = Some("myclient".to_string());
 
     // Get a handle to the deployment.
     let client = Client::with_options(client_options).unwrap();
-
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None, None).await {
-        println!("{:?}", db_name);
-    }
-    let db = client.database("PaulCoin");
+    let db = client.database("myclient");
 
     // List the names of the collections in that database.
     for collection_name in db.list_collection_names(None).await {
         println!("{:?}", collection_name);
     }
     // Get a handle to a collection in the database.
-    let collection = db.collection::<Document>("books");
+    let collection = db.collection::<Document>("block");
 
     let docs = vec![
-        doc! { "title": "1984", "author": "George Orwell" },
-        doc! { "title": "Animal Farm", "author": "George Orwell" },
-        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+        doc! { 
+            "transaction": "123",
+            "author": "Paul Cibier",
+            "Receiver" : "Remyx",
+            "previous_block": "000000000000000000000000000000000",
+            "hash_block" : "287Y87Y8Y8Y88T7TT8TUUGYGGHGHGHJGJ"
+        },
     ];
 
     // Insert some documents into the "mydb.books" collection.
