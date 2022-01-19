f866e3ef2c543ca027743a6362b680b82d95e660
paul.cibier <paul.cibier@gmail.com>
2022-01-19 17:47:34 +0100

    truc

--- src/mongo.rs
+++ src/mongo.rs
@@ -1,7 +1,22 @@
 use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::{doc, Document};
+use ring::{
+    rand,
+    signature::{self, KeyPair},
+};
 
 pub async fn init() {
+    let rng = rand::SystemRandom::new();
+    let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
+    let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+    const MESSAGE: &[u8] = b"hello, world";
+    let sig = key_pair.sign(MESSAGE);
+
+    let peer_public_key_bytes = key_pair.public_key().as_ref();
+    let peer_public_key =
+    signature::UnparsedPublicKey::new(&signature::ED25519, peer_public_key_bytes);
+    peer_public_key.verify(MESSAGE, sig.as_ref()).unwrap();
+
     // Parse a connection string into an options struct.
     let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await.unwrap();
 
